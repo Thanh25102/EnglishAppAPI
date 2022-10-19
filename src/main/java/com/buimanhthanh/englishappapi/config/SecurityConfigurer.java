154225a0e28407e719817a5dd3e58fe5b9071e43
manhthanh <manhthanh147@gmail.com>
2022-10-19 17:45:16 +0700

    update every incoming request for valid jwt and authorize

--- src/main/java/com/buimanhthanh/englishappapi/config/SecurityConfigurer.java
+++ src/main/java/com/buimanhthanh/englishappapi/config/SecurityConfigurer.java
@@ -1,22 +1,28 @@
 package com.buimanhthanh.englishappapi.config;
 
+import com.buimanhthanh.englishappapi.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @EnableWebSecurity
 public class SecurityConfigurer {
     @Autowired
-    public UserDetailsService userDetailsService;
-
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -27,7 +33,8 @@ public class SecurityConfigurer {
         return http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated()
-                .and().build();
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
